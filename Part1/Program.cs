048aba0e6652abaa53fb5c5ef2b1c398cf4543ba
ChesterBrock <cj.rozor@gmail.com>
2021-09-13 00:06:54 -0700

    First commit for part 1

--- Part1/Program.cs
+++ Part1/Program.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/// Chapter No. 2		Exercise No. 1
+/// File Name: Part1.java
+/// @author: Chester Brock 
+/// Date:  Sep 12, 2021
+///
+/// Problem Statement:  create a new string that contains your full name in pig latin 
+/// with the first letter capitalized for the first and last name. 
+/// 
+/// 
+/// Overall Plan:
+/// 1) string variable first set to your first name
+/// 2) string variable last set to your last name
+/// 3) Make sure its all lowercase
+/// 4) move the first letter to the back of the first and last name. 
+/// 5) add "ay" at the back of the first and last name 
+/// 6) Print the results. 
+///
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +28,22 @@ namespace Part1
     {
         static void Main(string[] args)
         {
+            string first = "chester";
+            string second = "brock";
+            string ending = "ay";
+
+            first = (first.Substring(1).ToUpper() + first[0] + ending);
+            // making all letters after first lowercase. 
+            first = (first.Substring(0).ToLower());
+
+            // Last name 
+            second = (second.Substring(1).ToUpper() + second[0] + ending);
+            second = (second.Substring(0).ToLower());
+
+
+            Console.WriteLine(first + " " + second); // the pig latin sentence.
+            // To pause the program
+            Console.ReadLine();
         }
     }
 }
---
+++ Part2/Program.cs
@@ -0,0 +1,31 @@
+﻿/// Chapter No. 1		Exercise No. 1
+/// File Name: MyFirstProgram.java
+/// @author: Chester Brock 
+/// Date:  August 24, 2021
+///
+/// Problem Statement: Ask the user to enter two numbers, calculate the sum of
+/// these two numbers, and display the sum to the screen
+/// 
+/// 
+/// Overall Plan:
+/// 1) Print an initial welcoming message to the screen
+/// 2) Prompt the user for two integers
+/// 3) Calculate the sum of the integers
+/// 4) Print the sum of the integers to the screen
+///
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
